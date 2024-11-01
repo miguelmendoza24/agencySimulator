b37a7e10225ec97706ff7b5a34b67afecf7f52d0
miguel <miguelevamendo@gmail.com>
2024-11-01 07:20:47 -0600

    añadir método actualizar a la clase administrador

--- Administrador.mjs
+++ Administrador.mjs
@@ -1,24 +1,10 @@
-class Administrador {
+export default class Administrador {
   constructor(nombre = "") {
     this.nombre = nombre;
     this.sugerenciasYQuejas = [];
   }
-  gestionarQueja(queja) {
-    this.sugerenciasYQuejas.push(queja);
-    console.log(`Queja o sugerencia recibida: "${queja}"`);
-  }
-  mostrarQuejas() {
-    if (this.sugerenciasYQuejas.length === 0) {
-      console.log("No se han recibido quejas ni sugerencias.");
-    } else {
-      console.log("Quejas y sugerencias recibidas:");
-      const resultado = "";
-      for (let index = 0; index < this.sugerenciasYQuejas.length; index++) {
-        resultado += `${index + 1}. ${this.sugerenciasYQuejas[index]}`
-      }
-      return resultado;
-    }
 
+  actualizar(nuevosDatos) {
+    Object.assign(this, nuevosDatos);
   }
-
 }
