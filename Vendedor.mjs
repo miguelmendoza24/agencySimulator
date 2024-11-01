fd49cd73ba85bb01c73350351adfda09af178f00
miguel <miguelevamendo@gmail.com>
2024-11-01 07:21:47 -0600

    añadir método actualizar a la clase vendedor

--- Vendedor.mjs
+++ Vendedor.mjs
@@ -7,13 +7,4 @@ export default class Vendedor {
   actualizar(nuevosDatos) {
     Object.assign(this, nuevosDatos);
   }
-
-  realizarVenta(cliente, auto) {
-    if (auto.disponible) {
-      cliente.comprarAuto(auto);
-      this.ventasRealizadas++;
-    } else {
-      console.log(`El auto ${auto.marca} ${auto.modelo} no está disponible.`);
-    }
-  }
 }
