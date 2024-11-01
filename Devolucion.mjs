25c7df86087dafc324e29351a5ecdc094606b0fa
miguel <miguelevamendo@gmail.com>
2024-11-01 07:19:48 -0600

    añadir método actualizar a la clase devoluciones

--- Devolucion.mjs
+++ Devolucion.mjs
@@ -1,14 +1,11 @@
-class Devolucion {
+export default class Devolucion {
   constructor(cliente = "") {
     this.cliente = cliente;
     this.auto = auto;
     this.fecha = fecha;
   }
-  mostrarInformacion() {
-    console.log(
-      `Devolución realizada por: ${this.cliente.nombre}
-Auto: ${this.auto.marca} ${this.auto.modelo}, Año: ${this.auto.año}
-Fecha: ${this.fecha}`
-    );
+
+  actualizar(nuevosDatos) {
+    Object.assign(this, nuevosDatos);
   }
 }
