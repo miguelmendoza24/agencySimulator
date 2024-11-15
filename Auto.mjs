e6c66f1efde4645b399615b9a4a94e586eedcdab
miguel <miguelevamendo@gmail.com>
2024-11-14 20:54:53 -0600

    Añadi dos métodos estáticos a la clase Auto

--- Auto.mjs
+++ Auto.mjs
@@ -10,4 +10,22 @@ export default class Auto {
   actualizar(nuevosDatos) {
     Object.assign(this, nuevosDatos);
   }
+
+  static buscarIndicePorPropiedad(autos, propiedad, valor) {
+    if (["año", "precio"].includes(propiedad)) {
+      valor = parseInt(valor);
+    }
+    return autos.findIndex((auto) => auto[propiedad] === valor);
+  }
+
+  static actualizarAutoPorPropiedad(autos, propiedad, valor, nuevosDatos) {
+    const indice = this.buscarIndicePorPropiedad(autos, propiedad, valor);
+
+    if (indice !== -1) {
+      autos[indice].actualizar(nuevosDatos);
+      return autos[indice];
+    }
+    return null;
+  }
 }
+
